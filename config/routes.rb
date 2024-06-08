b631a96d028e1cdad6517865c2d6528e5b3ffadb
Logan Finnegan <logantfinnegan@protonmail.com>
2024-06-08 10:29:06 -0500

    initial commit and setup

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+  namespace :api do
+    namespace :v1 do
+      resources :recipes, only: [:index]
+      resources :learning_resources, only: [:index]
+
+      resources :users, only: [:new, :create]
+      resources :sessions, only: [:new, :create]
+      resources :favorites, only: [:new, :create, :index]
+    end
+  end
+end
---
+++ spec/request/search_country.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'Request' do
+  before(:each) do
+
+  end
+
+  describe 'happy paths' do
+    # VCR.use_cassette('fixture name')
+      it 'What is my purpose?', :vcr do
+        headers = { 'CONTENT_TYPE': 'application/json', 'ACCEPT': 'application/json' }
+
+        get 'route', headers: headers
+
+        expect(response).to be_successful
+        expect(response.status).to eq(200)
+
+        market = JSON.parse(response.body, symbolize_names: true)[:data]
+
+        check_hash_structure(market, :id, String)
+        check_hash_structure(market, :attributes, Hash)
+        check_hash_structure(market, :type, String)
+        expect(market[:type]).to eq('market')
+
+        attrs = market[:attributes]
+
+        check_hash_structure(attrs, :name, String)
+        check_hash_structure(attrs, :street, String)
+        check_hash_structure(attrs, :city, String)
+      end
+  # end
+  end
+end
